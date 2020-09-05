40d32bd7a140688eda922630093535abbef5a3c9
Oleksandr Bevzytskyi <bealkl@gmail.com>
2020-09-05 13:00:18 +0300

    week release

---
+++ build/precache-manifest.349d5f307017ef6b6349d41034a78837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff4e6855f9389bad3325324b58abf50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cff54a4150e50f040297",
+    "url": "/static/css/main.446aa255.chunk.css"
+  },
+  {
+    "revision": "b3ad4886e7a31522001b",
+    "url": "/static/js/2.fc6ccb79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc6ccb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff54a4150e50f040297",
+    "url": "/static/js/main.19d66607.chunk.js"
+  },
+  {
+    "revision": "3fd7c41e85fe20a69193",
+    "url": "/static/js/runtime-main.1a265c15.js"
+  }
+]);
